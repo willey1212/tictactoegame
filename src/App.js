679035e4a2af61d2cee4e9e481b9dfb31e546c8a
Dave Bembenek <dave@followandlead.com>
2020-03-28 18:09:04 -0400

    tictactoe

--- src/App.js
+++ src/App.js
@@ -1,26 +1,134 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const Button = (props) => {
+    return (
+        <div className="button" onClick={() => props.clickHandler(props.itemKey,props.value)}>
+            <h2>{props.value}</h2>  
+        </div>
+    )
+}
+
+class App extends Component {
+
+
+    state = {
+        squares: [],
+        current: "x",
+        winner: false
+    }
+
+    componentDidMount() {
+        this.reset()
+    }
+
+    reset = () => {
+        this.setState({
+            squares: Array(9).fill(null),
+            current: "x",
+            winner: false
+        })
+    }
+
+    clickHandler = (k, v) => {
+
+
+
+        let currentState = this.state.squares
+        let currentPlayer = this.state.current
+
+        currentState.forEach((value, key) => {
+            if (k === key) {            
+                currentState.splice(k, 1, currentPlayer)
+            }
+        })        
+
+        this.setState({
+            squares: currentState,
+        })
+
+        this.checkWin()
+    }
+
+    checkWin = () => {
+        const squares = this.state.squares
+        const current = this.state.current
+        let winner = false
+
+        squares.map((e, i) => {
+
+            if (squares[0] === current && squares[1] === current && squares[2] === current) {
+                winner = true
+            } 
+            if (squares[3] === current && squares[4] === current && squares[5] === current) {
+                winner = true
+            } 
+            if (squares[6] === current && squares[7] === current && squares[8] === current) {
+                winner = true
+            } 
+            if (squares[0] === current && squares[3] === current && squares[6] === current) {
+                winner = true
+            } 
+            if (squares[1] === current && squares[4] === current && squares[7] === current) {
+                winner = true
+            } 
+            if (squares[2] === current && squares[5] === current && squares[8] === current) {
+                winner = true
+            } 
+            if (squares[0] === current && squares[4] === current && squares[8] === current) {
+                winner = true
+            } 
+
+
+        })
+
+        if (winner) {
+            this.setState({
+                winner: winner
+            })
+        } else {
+            this.changePlayer()
+        }
+
+    }
+
+    changePlayer = () => {
+      let currentPlayer = this.state.current
+
+      if (currentPlayer === "x") {
+            currentPlayer = "y"
+        } else {
+            currentPlayer = "x"
+        }
+
+      this.setState({
+          current: currentPlayer
+      })
+    }
+
+
+    render() {
+
+        const squares = this.state.squares
+        return (
+            <div className="container">
+            <p>The current player is: {this.state.current}</p>
+              <div className="holder">
+                {squares.map((e,i) => {
+                  return (
+                      <Button key={i} itemKey={i} clickHandler={this.clickHandler} value={e} />
+                    )
+                })
+              }
+              </div>
+                {this.state.winner && <p>Congrats player {this.state.current} you won!</p>}
+              <p onClick={this.reset}>reset</p>
+            </div>
+        );
+    }
+
 }
 
 export default App;
